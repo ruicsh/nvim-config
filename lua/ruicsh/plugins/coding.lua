28692bdad577befe5ce5d63cdf25aa8598e143a8
Rui Costa <ruicosta042@gmail.com>
2024-11-27 21:02:32 +0000

    fix(coding): only enable vim-illuminate on code files

--- lua/ruicsh/plugins/coding.lua
+++ lua/ruicsh/plugins/coding.lua
@@ -174,6 +174,26 @@ return {
 	{ -- Highlight matching words under cursor
 		-- https://github.com/RRethy/vim-illuminate
 		"rrethy/vim-illuminate",
+		opts = {
+			filetypes_allowlist = {
+				"css",
+				"dotenv",
+				"html",
+				"htmlangular",
+				"javascript",
+				"javascriptreact",
+				"json",
+				"lua",
+				"markdown",
+				"typescript",
+				"typescriptreact",
+			},
+			filetypes_denylist = {},
+		},
+		config = function(_, opts)
+			local illuminate = require("illuminate")
+			illuminate.configure(opts)
+		end,
 
 		event = { "VeryLazy" },
 	},
