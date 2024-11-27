6542fe810638c34849f95b3ab90b52886c12f2e3
Rui Costa <ruicosta042@gmail.com>
2024-11-27 21:51:40 +0000

    fix(keymaps): remove <c-r> mapping on insert mode

--- plugin/keymaps/insert.lua
+++ plugin/keymaps/insert.lua
@@ -1,7 +1,1 @@
 local k = vim.keymap.set
-
--- Paste with usual shortcut.
-k("i", "<c-v>", "<c-r>", { desc = "Paste" })
-
--- Navigation (alt-* keymaps from vim-rsi don't work on vscode/powershell).
-k("i", "<c-r>", "<c-r>+", { desc = "Paste" })
