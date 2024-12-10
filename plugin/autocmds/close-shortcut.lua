39a715cbbaa957993934b3cbad72a425435a82c9
Rui Costa <ruicosta042@gmail.com>
2024-12-10 14:05:29 +0000

    fix(autocmds): more filetypes for close-shortcut

--- plugin/autocmds/close-shortcut.lua
+++ plugin/autocmds/close-shortcut.lua
@@ -1,4 +1,5 @@
--- Use the same shortcut to close different panels
+-- Use the same shortcut to close different panels.
+-- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 
 local closeShortcut = "<c-]>"
 local api = vim.api
@@ -10,35 +11,50 @@ vim.keymap.set("n", closeShortcut, "<c-w>q", { desc = "Close split" })
 api.nvim_create_autocmd("FileType", {
 	group = group,
 	pattern = {
+		"checkhealth",
+		"fugitive",
 		"git",
+		"gitcommit",
 		"help",
+		"lspinfo",
 		"man",
 		"qf",
 		"query",
 		"scratch",
+		"spectre_panel",
+		"startuptime",
 	},
-	callback = function(args)
-		vim.keymap.set("n", closeShortcut, "<cmd>close<cr>", { buffer = args.buf })
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+		vim.schedule(function()
+			vim.keymap.set({ "i", "n", "x" }, closeShortcut, function()
+				vim.cmd("close")
+				pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+			end, {
+				buffer = event.buf,
+				silent = true,
+			})
+		end)
 	end,
 })
 
 api.nvim_create_autocmd("FileType", {
 	group = group,
 	pattern = { "neo-tree" },
-	callback = function(args)
-		vim.keymap.set("n", closeShortcut, "<cmd>Neotree action=close<cr>", { buffer = args.buf })
+	callback = function(event)
+		vim.keymap.set("n", closeShortcut, "<cmd>Neotree action=close<cr>", { buffer = event.buf })
 	end,
 })
 
 api.nvim_create_autocmd("FileType", {
 	group = group,
 	pattern = { "oil" },
-	callback = function(args)
+	callback = function(event)
 		vim.keymap.set(
 			"n",
 			closeShortcut,
 			"<cmd>lua require('oil.actions').close.callback()<cr>",
-			{ buffer = args.buf }
+			{ buffer = event.buf }
 		)
 	end,
 })
@@ -46,58 +62,23 @@ api.nvim_create_autocmd("FileType", {
 api.nvim_create_autocmd("FileType", {
 	group = group,
 	pattern = { "TelescopePrompt" },
-	callback = function(args)
+	callback = function(event)
 		local function close_telescope()
-			local bufnr = api.nvim_get_current_buf()
 			local actions = require("telescope.actions")
-			actions.close(bufnr)
+			actions.close(event.buf)
 		end
 
-		vim.keymap.set({ "i", "n" }, closeShortcut, close_telescope, { buffer = args.buf })
+		vim.keymap.set({ "i", "n" }, closeShortcut, close_telescope, { buffer = event.buf })
 	end,
 })
 
 api.nvim_create_autocmd("FileType", {
 	group = group,
-	pattern = { "DiffviewFiles" },
-	callback = function(args)
-		vim.keymap.set("n", closeShortcut, "<cmd>DiffviewClose<cr>", { buffer = args.buf })
-	end,
-})
-
-api.nvim_create_autocmd("FileType", {
-	group = group,
-	pattern = { "DiffviewFileHistory" },
-	callback = function(args)
-		vim.keymap.set("n", closeShortcut, "<cmd>tabclose<cr>", { buffer = args.buf })
-	end,
-})
-
-api.nvim_create_autocmd("FileType", {
-	group = group,
-	pattern = { "fugitive" },
-	callback = function(args)
-		vim.keymap.set("n", closeShortcut, "<plug>fugitive:gq", { noremap = true, buffer = args.buf })
-	end,
-})
-
-api.nvim_create_autocmd("FileType", {
-	group = group,
-	pattern = { "gitcommit" },
-	callback = function(args)
-		vim.keymap.set({ "n", "i" }, closeShortcut, "<cmd>q!<cr>", { noremap = true, buffer = args.buf })
-	end,
-})
-
-api.nvim_create_autocmd("FileType", {
-	group = group,
-	pattern = { "spectre_panel" },
-	callback = function(args)
-		vim.keymap.set(
-			{ "n", "i" },
-			closeShortcut,
-			"<esc><cmd>lua require('spectre').toggle()<cr>",
-			{ noremap = true, buffer = args.buf }
-		)
+	pattern = {
+		"DiffviewFiles",
+		"DiffviewFileHistory",
+	},
+	callback = function(event)
+		vim.keymap.set("n", closeShortcut, "<cmd>tabclose<cr>", { buffer = event.buf })
 	end,
 })
