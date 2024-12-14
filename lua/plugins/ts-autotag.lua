5d2e452aa82a85c1f4170c2c035ae0a9b78b2d44
Rui Costa <ruicosta042@gmail.com>
2024-12-14 19:32:10 +0000

    fix(plugins/ts-autotag): better config

--- lua/plugins/ts-autotag.lua
+++ lua/plugins/ts-autotag.lua
@@ -1,16 +1,26 @@
--- Auto-close/rename html tags
+-- Auto-close/rename html tags.
 -- https://github.com/windwp/nvim-ts-autotag
 
 return {
 	"windwp/nvim-ts-autotag",
 	options = {
 		opts = {
-			enable_close = true, -- Auto close tags.
-			enable_rename = true, -- Auto rename pairs of tags.
-			enable_close_on_slash = true, -- Auto close on trailing </.
+			enable_close = true,
+			enable_rename = true,
+			enable_close_on_slash = true,
+		},
+		aliases = {
+			["htmlangular"] = "html",
 		},
 	},
+	config = function(_, opts)
+		local autotag = require("nvim-ts-autotag")
+		autotag.setup(opts)
+	end,
 
-	main = "nvim-ts-autotag",
 	ft = { "html", "htmlangular", "typescript", "typescriptreact", "vue" },
+	event = { "VeryLazy" },
+	dependencies = {
+		{ "nvim-treesitter/nvim-treesitter" },
+	},
 }
