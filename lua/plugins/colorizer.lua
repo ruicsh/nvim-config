e166574e37e4abbef6db3ecc5453df031891c7c7
Rui Costa <ruicosta042@gmail.com>
2024-12-15 12:50:38 +0000

    fix(plugins): load colorizer on vimenter

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -14,17 +14,11 @@ return {
 			"typescriptreact",
 			"javascriptreact",
 		}, {
-			RGB = true,
-			RRGGBB = true,
-			names = true,
-			RRGGBBAA = true,
-			rgb_fn = true,
-			hsl_fn = true,
 			css = true,
 			css_fn = true,
 			mode = "background",
 		})
 	end,
 
-	event = { "VeryLazy" },
+	event = { "VimEnter" },
 }
