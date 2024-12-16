af965320c54861150b273aea7ad8f8640d28cfa1
Rui Costa <ruicosta042@gmail.com>
2024-12-16 11:13:33 +0000

    fix(terminal): open buffer switch from inside terminal

--- plugin/autocmds/terminal.lua
+++ plugin/autocmds/terminal.lua
@@ -10,18 +10,6 @@ vim.api.nvim_create_autocmd("TermOpen", {
 			vim.opt_local.number = false -- Don't show numbers
 			vim.opt_local.relativenumber = false -- Don't show relativenumbers
 			vim.cmd.startinsert() -- Start insert mode
-			vim.bo.filetype = "terminal" -- Set correct filetype
-		end
-	end,
-})
-
--- When switching to a terminal window, always start insert mode
-vim.api.nvim_create_autocmd("BufEnter", {
-	group = group,
-	pattern = "term://*",
-	callback = function()
-		if vim.fn.mode() ~= "i" then
-			vim.cmd.startinsert()
 		end
 	end,
 })
--- plugin/keymaps/terminal.lua
+++ plugin/keymaps/terminal.lua
@@ -1,1 +1,8 @@
+local k = vim.keymap.set
 
+-- Open buffer switcher from inside terminal
+k("t", "§", function()
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<c-\\><c-n>", true, true, true), "n", true)
+	local bmui = require("buffer_manager.ui")
+	bmui.toggle_quick_menu()
+end, { silent = true })
