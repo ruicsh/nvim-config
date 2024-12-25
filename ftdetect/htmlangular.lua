b8ae5962df47d7105b6a52089f3b91428520d3ae
Rui Costa <ruicosta042@gmail.com>
2024-12-25 12:17:59 +0000

    refactor(ftplugin): rename dir

--- ftdetect/htmlangular.lua
+++ ftdetect/htmlangular.lua
@@ -1,6 +1,9 @@
 -- Detect and set the proper file type for Angular Template files.
+
+local group = vim.api.nvim_create_augroup("ruicsh/ftdetect/htmlangular", { clear = true })
+
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-	group = vim.api.nvim_create_augroup("ruicsh/htmlangular", { clear = true }),
+	group = group,
 	pattern = "*.component.html",
 	callback = function()
 		vim.bo.filetype = "htmlangular"
