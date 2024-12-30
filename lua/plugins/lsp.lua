e516a2741fbc44aaa1805d29e7596f070cebec00
Rui Costa <ruicosta042@gmail.com>
2024-12-30 13:05:13 +0000

    fix(lsp): conditional keymaps on client capabilities

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -28,7 +28,7 @@ local function lsp_on_attach(client, bufnr)
 	local methods = vim.lsp.protocol.Methods
 
 	-- Highlight all references to symbol under cursor
-	if client:supports_method(methods.textDocument_documentHighlight) then
+	if client.supports_method(methods.textDocument_documentHighlight) then
 		local group = vim.api.nvim_create_augroup("ruicsh/lsp_document_highlight", { clear = true })
 		vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
 
@@ -48,7 +48,7 @@ local function lsp_on_attach(client, bufnr)
 	end
 
 	-- Inlay hints
-	if client:supports_method(methods.textDocument_inlayHint) then
+	if client.supports_method(methods.textDocument_inlayHint) then
 		local function toggle_hint()
 			local enabled = vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr })
 			vim.lsp.inlay_hint.enable(not enabled, { bufnr = bufnr })
@@ -72,20 +72,45 @@ local function lsp_on_attach(client, bufnr)
 
 	-- Keymaps
 	-- https://neovim.io/doc/user/lsp.html#lsp-defaults
-	k("K", vim.lsp.buf.hover, "Hover")
-	k("gd", vim.lsp.buf.definition, "Jump to [d]efinition")
-	k("gD", vim.lsp.buf.declaration, "Jump to [D]eclaration")
-	k("gO", vim.lsp.buf.document_symbol, "Document Symbol")
-	k("gra", vim.lsp.buf.code_action, "Code actions", { "n", "v" })
-	k("grt", vim.lsp.buf.type_definition, "Jump to type definition")
-	k("gri", vim.lsp.buf.implementation, "List [i]mplementations")
-	k("grj", vim.lsp.buf.incoming_calls, "Incoming calls")
-	k("grk", vim.lsp.buf.outgoing_calls, "Outgoing calls")
-	k("grn", vim.lsp.buf.rename, "Rename symbol")
-	k("grr", vim.lsp.buf.references, "List [r]eferences")
-	k("<c-s>", vim.lsp.buf.signature_help, "Signature help", { "n", "i" })
-
-	k("<leader>dd", vim.diagnostic.setqflist, "Diagnostics")
+	if client.supports_method(methods.textDocument_hover) then
+		k("K", vim.lsp.buf.hover, "Hover")
+	end
+	if client.supports_method(methods.textDocument_definition) then
+		k("gd", vim.lsp.buf.definition, "Jump to [d]efinition")
+	end
+	if client.supports_method(methods.textDocument_declaration) then
+		k("gD", vim.lsp.buf.declaration, "Jump to [D]eclaration")
+	end
+	if client.supports_method(methods.textDocument_documentSymbol) then
+		k("gO", vim.lsp.buf.document_symbol, "Document Symbol")
+	end
+	if client.supports_method(methods.textDocument_codeAction) then
+		k("gra", vim.lsp.buf.code_action, "Code actions", { "n", "v" })
+	end
+	if client.supports_method(methods.textDocument_typeDefinition) then
+		k("grt", vim.lsp.buf.type_definition, "Jump to type definition")
+	end
+	if client.supports_method(methods.textDocument_implementation) then
+		k("gri", vim.lsp.buf.implementation, "List [i]mplementations")
+	end
+	if client.supports_method(methods.callHierarchy_incomingCalls) then
+		k("grj", vim.lsp.buf.incoming_calls, "Incoming calls")
+	end
+	if client.supports_method(methods.callHierarchy_outgoingCalls) then
+		k("grk", vim.lsp.buf.outgoing_calls, "Outgoing calls")
+	end
+	if client.supports_method(methods.textDocument_rename) then
+		k("grn", vim.lsp.buf.rename, "Rename symbol")
+	end
+	if client.supports_method(methods.textDocument_references) then
+		k("grr", vim.lsp.buf.references, "List [r]eferences")
+	end
+	if client.supports_method(methods.textDocument_signatureHelp) then
+		k("<c-s>", vim.lsp.buf.signature_help, "Signature help", { "n", "i" })
+	end
+	if client.supports_method(methods.workspace_diagnostic) then
+		k("<leader>dd", vim.diagnostic.setqflist, "Diagnostics")
+	end
 end
 
 return {
