2ac76a66b6917c12ac19df1ee63ccd28d7215fc4
Rui Costa <ruicosta042@gmail.com>
2025-01-10 23:57:59 +0000

    refactor(autocmds): add pattern to termopen

--- plugin/autocmds/terminal.lua
+++ plugin/autocmds/terminal.lua
@@ -5,11 +5,15 @@ local group = vim.api.nvim_create_augroup("ruicsh/terminal", { clear = true })
 
 vim.api.nvim_create_autocmd("TermOpen", {
 	group = group,
+	pattern = "term://*",
 	callback = function()
 		if vim.opt.buftype:get() == "terminal" then
-			vim.opt_local.number = false -- Don't show numbers
-			vim.opt_local.relativenumber = false -- Don't show relativenumbers
-			vim.cmd.startinsert() -- Start insert mode
+			local set = vim.opt_local
+			set.number = false -- Don't show numbers
+			set.relativenumber = false -- Don't show relativenumbers
+			set.scrolloff = 0 -- Don't scroll when at the top or bottom of the terminal buffer
+
+			vim.cmd.startinsert() -- Start in insert mode
 		end
 	end,
 })
