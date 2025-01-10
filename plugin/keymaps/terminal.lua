7d73b7cd7948465e48039da897c5ff63b83612c8
Rui Costa <ruicosta042@gmail.com>
2025-01-10 22:30:34 +0000

    fix(terminal): paste from register

--- plugin/keymaps/terminal.lua
+++ plugin/keymaps/terminal.lua
@@ -3,9 +3,12 @@ local function k(lhs, rhs, opts)
 	vim.keymap.set("t", lhs, rhs, options)
 end
 
--- Open buffer switcher from inside terminal
+-- Open buffer switcher from inside terminal.
 k("§", function()
 	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<c-\\><c-n>", true, true, true), "n", true)
 	local bmui = require("buffer_manager.ui")
 	bmui.toggle_quick_menu()
 end, { silent = true })
+
+-- Paste from register in terminal.
+k("<c-r>", [['<c-\><c-n>"'.nr2char(getchar()).'pi']], { expr = true })
