e85483fe50418ce5c8edc79ac704ff0830c9e251
Rui Costa <ruicosta042@gmail.com>
2025-01-13 23:14:00 +0000

    fix(plugins/autopairs): don't close pair if there's a char on the right

--- lua/plugins/mini.pairs.lua
+++ lua/plugins/mini.pairs.lua
@@ -3,7 +3,22 @@
 
 return {
 	"echasnovski/mini.pairs",
-	opts = {},
+	opts = {
+		mappings = {
+			-- Don't close pairs if there's a character on the right
+			["("] = { action = "open", pair = "()", neigh_pattern = "[^\\][%W]" },
+			["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\][%W]" },
+			["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\][%W]" },
 
-	event = { "VeryLazy" },
+			[")"] = { action = "close", pair = "()", neigh_pattern = "[^\\][%W]" },
+			["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\][%W]" },
+			["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\][%W]" },
+
+			['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\][%W]", register = { cr = false } },
+			["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\][%W]", register = { cr = false } },
+			["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\][%W]", register = { cr = false } },
+		},
+	},
+
+	event = { "InsertEnter" },
 }
