1ba9b6c045ba2b94b4d41788cdb3285f8030c706
Rui Costa <ruicosta042@gmail.com>
2025-01-17 09:21:47 +0000

    fix(terminal): use default window switch on terminal windows

--- plugin/autocmds/yank-keep-cursor-position.lua
+++ plugin/autocmds/yank-keep-cursor-position.lua
@@ -11,7 +11,7 @@ end, { expr = true, silent = true, unique = true })
 
 vim.keymap.set("n", "Y", function()
 	cursorPreYank = vim.api.nvim_win_get_cursor(0)
-	return "y$"
+	return "yg_" -- don't include whitespaces at the end
 end, { expr = true, silent = true })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
--- plugin/keymaps/terminal.lua
+++ plugin/keymaps/terminal.lua
@@ -9,3 +9,12 @@ k("§", function()
 	local bmui = require("buffer_manager.ui")
 	bmui.toggle_quick_menu()
 end, { silent = true })
+
+-- Leave terminal window with <C-w>hjkl.
+local code_term_esc = vim.api.nvim_replace_termcodes("<C-\\><C-n>", true, true, true)
+for _, key in ipairs({ "h", "j", "k", "l" }) do
+	k("<C-w>" .. key, function()
+		local code_dir = vim.api.nvim_replace_termcodes("<C-w>" .. key, true, true, true)
+		vim.api.nvim_feedkeys(code_term_esc .. code_dir, "t", true)
+	end, { noremap = true })
+end
