3c9701f41a56ca908c309aff0ea2016fef51e648
Rui Costa <ruicosta042@gmail.com>
2025-01-27 13:39:39 +0000

    fix(autocmds): don't save some filetypes when leaving window

--- plugin/autocmds/save-on-win-leave.lua
+++ plugin/autocmds/save-on-win-leave.lua
@@ -2,9 +2,17 @@
 
 local augroup = vim.api.nvim_create_augroup("ruicsh/autocmds/save-on-win-leave", { clear = true })
 
+local ignore_ft = { "", "oil", "fugitive", "gitcommit", "snacks_picker_input", "help", "qf", "terminal" }
+
 vim.api.nvim_create_autocmd("WinLeave", {
 	group = augroup,
 	callback = function(event)
+		local ft = vim.bo.filetype
+
+		if vim.tbl_contains(ignore_ft, ft) then
+			return
+		end
+
 		if vim.bo.modified then
 			require("conform").format({ bufnr = event.buf })
 			vim.cmd("write")
