dfff9797b130623368e7c11680ab8f023449b205
Rui Costa <ruicosta042@gmail.com>
2025-02-02 12:37:48 +0000

    feat(snacks.picker): search in directory

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -14,9 +14,49 @@ return {
 			snacks.words.jump(1, true)
 		end
 
+		local search_word = function()
+			local word = vim.fn.expand("<cword>")
+			snacks.picker.grep({ search = word })
+		end
+
+		local dirs_to_exclude_from_search = {
+			".git",
+			".requests",
+			".turbo",
+			"__data__",
+			"node_modules",
+		}
+
+		local search_directory = function()
+			local conditions = {}
+			local gitignore_dirs = vim.fn.systemlist("git ls-files --others --ignored --exclude-standard --directory")
+			for _, dir in ipairs(gitignore_dirs) do
+				-- anwywhere in the path
+				table.insert(conditions, "-path '*" .. dir .. "*'")
+			end
+			for _, dir in ipairs(dirs_to_exclude_from_search) do
+				-- exclude by name, exact match
+				table.insert(conditions, "-name " .. dir)
+			end
+			local exclude = table.concat(conditions, " -o ")
+			local cmd = "find . -type d \\( " .. exclude .. " \\) -prune -o -type d -print | sort"
+			local dirs = vim.fn.systemlist(cmd)
+
+			snacks.picker.select(dirs, {
+				prompt = "Select a directory",
+				format_item = function(item)
+					return item
+				end,
+			}, function(choice)
+				snacks.picker.grep({ dirs = { choice } })
+			end)
+		end
+
 		local mappings = {
 			{ "<leader><leader>", snacks.picker.files, "Search: Files" },
-			{ "<leader>f", snacks.picker.grep, "Search: Workspace" },
+			{ "<leader>ff", snacks.picker.grep, "Search: Workspace" },
+			{ "<leader>fw", search_word, "Search: Word under cursor" },
+			{ "<leader>fd", search_directory, "Search: Directory" },
 			{ "<leader>,", snacks.picker.buffers, "Search: Buffers" },
 			{ "<leader>j", snacks.picker.jumps, "Search: Jumplist" },
 			{ "<leader>nh", snacks.picker.help, "Search: Help" },
