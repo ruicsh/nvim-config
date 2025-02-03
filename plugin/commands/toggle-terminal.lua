bad98a6e5dc6e3d946e9b7de6489245862d6bef9
Rui Costa <ruicosta042@gmail.com>
2025-02-03 23:33:15 +0000

    refactor(toggle-terminal): no need for silent cmd

--- plugin/commands/toggle-terminal.lua
+++ plugin/commands/toggle-terminal.lua
@@ -1,4 +1,4 @@
--- Hhave a single terminal buffer that can be toggled with a command
+-- Have a single terminal buffer that can be toggled with a command
 -- https://github.com/jaimecgomezz/here.term/
 
 local current_buf = nil
@@ -7,14 +7,14 @@ local terminal_buf = nil
 local function exit_terminal()
 	-- enter the buffer that was active before the terminal
 	if vim.fn.buflisted(current_buf) == 1 and current_buf ~= terminal_buf then
-		vim.cmd("buffer" .. current_buf)
+		vim.cmd.buffer(current_buf)
 		return
 	end
 
 	-- if the current buffer is the terminal buffer, then go to the previous buffer
 	for _, buffer in ipairs(vim.api.nvim_list_bufs()) do
 		if vim.fn.buflisted(buffer) == 1 and buffer ~= terminal_buf then
-			vim.cmd("silent buffer" .. buffer)
+			vim.cmd.buffer(buffer)
 			return
 		end
 	end
@@ -22,10 +22,9 @@ end
 
 local function enter_terminal()
 	if vim.fn.bufexists(terminal_buf) == 1 then
-		vim.cmd("silent buffer" .. terminal_buf)
-		-- vim.cmd.startinsert()
+		vim.cmd.buffer(terminal_buf)
 	else
-		vim.cmd("silent terminal")
+		vim.cmd.terminal()
 		terminal_buf = vim.api.nvim_get_current_buf()
 	end
 end
