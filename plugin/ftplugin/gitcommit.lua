c7b4162122db12c95f5c32c3cfd0d2de2f601c2b
Rui Costa <ruicosta042@gmail.com>
2025-02-11 16:49:13 +0000

    feat: Save commit message with whitespace trimmed
    This commit introduces a function to save the commit message
    after trimming leading whitespaces. This ensures a clean and
    consistent commit message format.

--- plugin/ftplugin/gitcommit.lua
+++ plugin/ftplugin/gitcommit.lua
@@ -2,6 +2,11 @@
 
 local augroup = vim.api.nvim_create_augroup("ruicsh/ft/gitcommit", { clear = true })
 
+local save_commit_message = function()
+	vim.cmd("%s/^\\s*//g") -- trim leading whitespace(s)
+	vim.cmd("wq")
+end
+
 vim.api.nvim_create_autocmd("FileType", {
 	group = augroup,
 	pattern = "gitcommit",
@@ -13,6 +18,6 @@ vim.api.nvim_create_autocmd("FileType", {
 		local k = vim.keymap.set
 		local opts = { buffer = event.buf }
 		k({ "n", "i" }, "<c-m>", "<esc>:CopilotChatGenCommitMessage<cr>", opts) -- Generate commit message.
-		k({ "n", "i" }, "<c-s>", "<esc>:%s/^\\s\\+//g<cr>:w<cr>:wq<cr>", opts) -- Save commit message.
+		k({ "n", "i" }, "<c-s>", save_commit_message, opts)
 	end,
 })
