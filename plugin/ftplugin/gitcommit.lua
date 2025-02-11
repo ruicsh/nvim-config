7387cda37e072abfa20dda4f6438d57352631c23
Rui Costa <ruicosta042@gmail.com>
2025-02-11 16:39:36 +0000

    fix: Remove leading whitespace globally in commit message
     The previous implementation only removed the first instance of
     leading whitespace. This change updates the substitution to remove
     all instances of leading whitespace.

--- plugin/ftplugin/gitcommit.lua
+++ plugin/ftplugin/gitcommit.lua
@@ -13,6 +13,6 @@ vim.api.nvim_create_autocmd("FileType", {
 		local k = vim.keymap.set
 		local opts = { buffer = event.buf }
 		k({ "n", "i" }, "<c-m>", "<esc>:CopilotChatGenCommitMessage<cr>", opts) -- Generate commit message.
-		k({ "n", "i" }, "<c-s>", "<esc>:%s/^\\s\\+//<cr>:w<cr><c-w>q", opts) -- Save commit message.
+		k({ "n", "i" }, "<c-s>", "<esc>:%s/^\\s\\+//g<cr>:w<cr><c-w>q", opts) -- Save commit message.
 	end,
 })
