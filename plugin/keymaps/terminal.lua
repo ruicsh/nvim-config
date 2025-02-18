0248490083054d5d63f3efd32dba9efac5555670
Rui Costa <ruicosta042@gmail.com>
2025-02-18 09:36:39 +0000

    refactor(keymaps): remove terminal buffer switcher key
    The buffer switcher shortcut in the terminal mode, bound to '§', has been removed to simplify the terminal keymaps and reduce potential conflicts. This removal is part of an effort to maintain a cleaner and more focused terminal keymap configuration.

--- plugin/keymaps/terminal.lua
+++ plugin/keymaps/terminal.lua
@@ -3,13 +3,6 @@ local function k(lhs, rhs, opts)
 	vim.keymap.set("t", lhs, rhs, options)
 end
 
--- Open buffer switcher from inside terminal.
-k("§", function()
-	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<c-\\><c-n>", true, true, true), "n", true)
-	local bmui = require("buffer_manager.ui")
-	bmui.toggle_quick_menu()
-end, {})
-
 -- Toggle terminal open/close
 k("<c-\\>", function()
 	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<c-\\><c-n>", true, true, true), "n", true)
