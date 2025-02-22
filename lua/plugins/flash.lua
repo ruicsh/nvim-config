1510fcda0f00843eb7225e8864d0a1c3936a25b7
Rui Costa <ruicosta042@gmail.com>
2025-02-22 18:26:04 +0000

    remove(keymaps): delete treesitter search mapping
    This commit removes the treesitter search mapping and configuration from the flash.lua plugin. The `<c-v>` keybinding and related treesitter_search configuration are no longer needed as the main treesitter functionality is already available through `<leader>v`.

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -9,7 +9,6 @@ return {
 		local mappings = {
 			{ "s", flash.jump, "Jump", { mode = { "n", "o", "v" } } },
 			{ "<leader>v", flash.treesitter, "Treesitter", { mode = { "n", "x", "o" } } },
-			{ "<c-v>", flash.treesitter_search, "Treesitter Search", { mode = "n" } },
 		}
 
 		return vim.fn.get_lazy_keys_conf(mappings, "Flash")
@@ -37,14 +36,6 @@ return {
 					autojump = false,
 					pos = "range",
 				},
-				label = {
-					before = true,
-					after = false,
-					style = "overlay",
-				},
-			},
-			treesitter_search = {
-				enabled = true,
 				label = {
 					before = true,
 					after = false,
