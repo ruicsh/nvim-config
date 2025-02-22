51aa8004f663177d6cb4f12c5df68027156ba984
Rui Costa <ruicosta042@gmail.com>
2025-02-22 23:03:09 +0000

    fix: improve scroll-eof behavior with folds & scrolloff
    * improve scroll handling when lines are folded by accounting for folded line amounts
    * fix issue where scrolloff sometimes gets reset to 0 on new buffers by adding auto-restoration
    * add attribution to source code inspiration

--- plugin/autocmds/scroll-eof.lua
+++ plugin/autocmds/scroll-eof.lua
@@ -1,5 +1,6 @@
 -- Scroll past the end of file just like scrolloff option.
 -- simplified version of https://github.com/Aasim-A/scrollEOF.nvim
+-- https://github.com/chrisgrieser/.config/blob/main/nvim/lua/config/autocmds.lua
 
 local augroup = vim.api.nvim_create_augroup("ruicsh/autocmds/scroll-eof", { clear = true })
 
@@ -13,11 +14,31 @@ vim.api.nvim_create_autocmd({ "CursorMoved", "WinScrolled" }, {
 		local visual_distance_to_eof = win_height - win_cur_line
 
 		if visual_distance_to_eof < scrolloff then
-			local win_view = vim.fn.winsaveview()
+			local topline = vim.fn.winsaveview().topline
+			-- topline is inaccurate if it is a folded line, thus add number of folded lines
+			local toplineFoldAmount = vim.fn.foldclosedend(topline) - vim.fn.foldclosed(topline)
+			topline = topline + toplineFoldAmount
+
 			vim.fn.winrestview({
 				skipcol = 0, -- Without this, `gg` `G` can cause the cursor position to be shown incorrectly
-				topline = win_view.topline + scrolloff - visual_distance_to_eof,
+				topline = topline + scrolloff - visual_distance_to_eof,
 			})
 		end
+	end,
+})
+
+-- FIX for some reason `scrolloff` sometimes being set to `0` on new buffers
+local originalScrolloff = vim.o.scrolloff
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufNew" }, {
+	callback = function(ctx)
+		vim.defer_fn(function()
+			if not vim.api.nvim_buf_is_valid(ctx.buf) or vim.bo[ctx.buf].buftype ~= "" then
+				return
+			end
+
+			if vim.o.scrolloff == 0 then
+				vim.o.scrolloff = originalScrolloff
+			end
+		end, 150)
 	end,
 })
