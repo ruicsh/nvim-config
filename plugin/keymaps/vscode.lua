10a5dc9aca4d3ecdf3f8f0e4fabc25bb54653f54
Rui Costa <ruicosta042@gmail.com>
2025-04-11 11:07:24 +0100

    feat(keymaps): update and streamline vscode keybindings
    Removed redundant buffer navigation and Copilot keybindings. Adjusted symbol navigation shortcuts for better usability.

--- plugin/keymaps/vscode.lua
+++ plugin/keymaps/vscode.lua
@@ -19,10 +19,6 @@ k("n", "<leader>ee", ac("workbench.files.action.focusFilesExplorer"))
 k("n", "<leader>ff", ac("workbench.action.findInFiles"))
 
 -- Buffers
-k("n", "[b", ac("workbench.action.previousEditorInGroup"))
-k("n", "]b", ac("workbench.action.nextEditorInGroup"))
-k("n", "<bs>", ac("workbench.action.openPreviousRecentlyUsedEditor"))
-k("n", "<s-bs>", ac("workbench.action.openNextRecentlyUsedEditor"))
 k("n", "<leader>,", ac("workbench.action.showAllEditors"))
 
 -- Splits
@@ -35,8 +31,7 @@ k("n", "<c-w>]", ac("workbench.action.moveEditorToRightGroup"))
 k("n", "<cr>", ac("editor.action.revealDefinition"))
 k("n", "grt", ac("editor.action.goToTypeDefinition"))
 k("n", "grr", ac("references-view.findReferences"))
-k("n", "gy", ac("workbench.action.gotoSymbol"))
-k("n", "gO", ac("workbench.action.showAllSymbols"))
+k("n", "gO", ac("workbench.action.gotoSymbol"))
 k("n", "[r", ac("editor.action.wordHighlight.prev"))
 k("n", "]r", ac("editor.action.wordHighlight.next"))
 
@@ -45,10 +40,3 @@ k("n", "<tab>", ac("editor.toggleFold"))
 
 -- Git
 k({ "n", "x" }, "<leader>hh", ac("workbench.view.scm"))
-
--- Copilot
-k("n", "<leader>aa", ac("workbench.action.openQuickChat"))
-k("v", "<leader>ae", ac("github.copilot.chat.explain"))
-k("v", "<leader>af", ac("github.copilot.chat.fix"))
-k("v", "<leader>ar", ac("github.copilot.chat.review"))
-k("v", "<leader>ar", ac("github.copilot.chat.generateTests"))
