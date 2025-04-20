02d50766a84294f95f914b894a1c3a8ec9393822
Rui Costa <ruicosta042@gmail.com>
2025-04-20 16:32:45 +0100

    feat(fugitive): improve git status buffer handling
    Ensure Fugitive buffer reuse if already open, otherwise open Git status in a vertical split and move it to the right edge.

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -5,8 +5,20 @@ return {
 	"tpope/vim-fugitive",
 	keys = function()
 		local function open_git_status()
-			vim.cmd("only")
-			vim.cmd("vertical Git")
+			local buf_id = vim.fn.bufnr("^fugitive://")
+			-- Fugitive is already open, so just switch to it
+			if buf_id ~= -1 then
+				local win_ids = vim.fn.win_findbuf(buf_id)
+				if #win_ids > 0 then
+					vim.api.nvim_set_current_win(win_ids[1])
+					return
+				end
+			end
+
+			-- Fugitive is not open, so we need to open it
+			vim.cmd("only") -- Make sure we are in a single window
+			vim.cmd("vertical Git") -- Open Git status in a vertical split
+			vim.cmd("wincmd L") -- Send current window (fugitive) to the right edge
 		end
 
 		local mappings = {
