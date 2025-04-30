815e4738caa8bc638ca81abc3d56d5bd79d7ae18
Rui Costa <ruicosta042@gmail.com>
2025-04-30 10:56:28 +0100

    feat(gitcommit): enhance line wrapping and keybinding
    Enable line wrapping for better readability. Update keybinding from <c-y> to <c-s> for accepting commit messages.

--- after/ftplugin/gitcommit.lua
+++ after/ftplugin/gitcommit.lua
@@ -1,10 +1,10 @@
-vim.opt_local.modifiable = true
-vim.opt_local.textwidth = 0 -- Disable text width
-vim.opt_local.wrapmargin = 0 -- Disable wrap margin
 vim.opt_local.linebreak = true -- Break lines at word boundaries
+vim.opt_local.modifiable = true
 vim.opt_local.number = false
 vim.opt_local.relativenumber = false
 vim.opt_local.signcolumn = "yes:1"
+vim.opt_local.textwidth = 0 -- Disable text width
+vim.opt_local.wrap = true -- Enable line wrapping
 
 local k = vim.keymap.set
 local opts = { buffer = 0 }
@@ -14,5 +14,5 @@ local function accept_commit_message()
 	vim.cmd("wq")
 end
 
-k({ "n", "i" }, "<c-y>", accept_commit_message, opts)
+k({ "n", "i" }, "<c-s>", accept_commit_message, opts)
 k({ "n", "i" }, "<leader>ac", ":CopilotCommitMessage<cr>", opts)
