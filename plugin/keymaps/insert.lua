f77bc001e80a5b8c415f63db2a0f1656903ac39d
Rui Costa <ruicosta042@gmail.com>
2025-06-30 07:37:04 +0100

    feat(insert): update insert mode keybindings
    - Added comments to clarify keybindings for cursor movement.
    - Introduced a new keybinding for pasting from clipboard without auto-indentation.
    - Updated documentation for existing keybindings for better usability.

--- plugin/keymaps/insert.lua
+++ plugin/keymaps/insert.lua
@@ -11,16 +11,16 @@ for _, key in ipairs(save_keys) do
 	k(key, "<cmd>silent! update<cr><esc>", { desc = "Save", unique = false })
 end
 
--- Jump character back/forward
-k("<c-b>", "<left>")
-k("<c-f>", "<right>")
+k("<c-b>", "<left>") -- Cursor one character left `:h i_<Left>`
+k("<c-f>", "<right>") -- Cursor one character right `:h i_<Right>`
+k("<c-v>", "<c-r><c-o>+") -- Paste from clipboard without auto-indentation (`:h i_CTRL-r_CTRL-O`)
 
 -- <c-w> Delete word before cursor (`:h i_CTRL-W`)
 -- <c-u> Delete all text before cursor (`:h i_CTRL-U`)
 -- <c-a> Insert previously insert text (`:h i_CTRL-A`)
 -- <c-r><c-o> Don't auto-indent when pasting (`:h i_CTRL-R_CTRL-O`)
 -- <c-t> Insert one shiftwidth of indentation (`:h i_CTRL-T`)
--- <c-d> Remove one shiftwidth of indentation (`:h i_CTRL-D`)
+-- <c-d> Delete one shiftwidth of indentation (`:h i_CTRL-D`)
 
 -- Make undo work word by word (`:h i_CTRL-G_u`)
 local undo_keys = { "<space>", ",", ".", "!", "?", ">", ")", "]", "}" }
