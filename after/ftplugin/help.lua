79bb584cf39437b0424aad19db02101e5dcde3f9
Rui Costa <ruicosta042@gmail.com>
2025-08-23 20:28:52 +0100

    fix(help) close with `q` but not window

--- after/ftplugin/help.lua
+++ after/ftplugin/help.lua
@@ -5,5 +5,8 @@ local opts = { buffer = true }
 
 -- Better navigation.
 -- https://vim.fandom.com/wiki/Learn_to_use_help#Simplify_help_navigation
-k("n", "<cr>", "<c-]>", opts)
-k("n", "<bs>", "<c-T>", opts)
+k("n", "<cr>", "<c-]>", opts) -- `:h ctrl-]`
+k("n", "<bs>", "<c-T>", opts) -- `:h ctrl-t`
+
+-- Close help with `q` but not the window
+k("n", "q", require("snacks").bufdelete.delete, opts)
