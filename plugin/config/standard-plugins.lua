7ec42df9208d55642339493f7bb66341db22f386
Rui Costa <ruicosta042@gmail.com>
2025-08-30 15:47:40 +0100

    refactor(plugins) disable standard plugins

--- plugin/config/standard-plugins.lua
+++ plugin/config/standard-plugins.lua
@@ -1,27 +1,35 @@
-local g = vim.g
-
 -- Disable unused built-in plugins
-g.loaded_2html_plugin = 1
-g.loaded_getscript = 1
-g.loaded_getscriptPlugin = 1
-g.loaded_gzip = 1
--- g.loaded_matchparen = 1
-g.loaded_netrwPlugin = 1
-g.loaded_rrhelper = 1
-g.loaded_tar = 1
-g.loaded_tarPlugin = 1
-g.loaded_tutor_mode_plugin = 1
-g.loaded_vimball = 1
-g.loaded_vimballPlugin = 1
-g.loaded_zip = 1
-g.loaded_zipPlugin = 1
+local disabled_plugins = {
+	"2html_plugin",
+	"getscript",
+	"getscriptPlugin",
+	"gzip",
+	"netrw",
+	"netrwPlugin",
+	"rrhelper",
+	"tar",
+	"tarPlugin",
+	"tutor_mode_plugin",
+	"vimball",
+	"vimballPlugin",
+	"zip",
+	"zipPlugin",
+}
+for _, plugin in pairs(disabled_plugins) do
+	vim.g["loaded_" .. plugin] = 1
+end
 
 -- Disable unused remote plugin providers
-g.loaded_python_provider = 0
-g.loaded_python3_provider = 0
-g.loaded_node_provider = 0
-g.loaded_perl_provider = 0
-g.loaded_ruby_provider = 0
+local disable_providers = {
+	"node",
+	"perl",
+	"python",
+	"python3",
+	"ruby",
+}
+for _, provider in pairs(disable_providers) do
+	vim.g["loaded_" .. provider .. "_provider"] = 0
+end
 
 -- matchparen `:h matchparen`
-g.matchparen_disable_cursor_hl = 1
+vim.g.matchparen_disable_cursor_hl = 1
