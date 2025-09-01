591590f82f5dc66b7eb1c0aa6e53e035fc7eee01
Rui Costa <ruicosta042@gmail.com>
2025-09-01 20:00:56 +0100

    docs(options) add help annotation

--- plugin/options.lua
+++ plugin/options.lua
@@ -3,25 +3,26 @@
 local o = vim.opt
 
 -- 2 moving around, searching and patterns {{{
-o.jumpoptions = {
+o.jumpoptions = { -- `:h 'jumpoptions'`
 	"stack", -- https://www.reddit.com/r/neovim/comments/16nead7/comment/k1e1nj5/?context=3
 	"view", -- https://www.reddit.com/r/neovim/comments/1kfcrfx/how_can_lazyvim_mark_also_save_the_scroll_position/
 }
-o.ignorecase = true -- Ignore case on search patterns.
-o.inccommand = "nosplit" -- Show live preview of substitution.
-o.path:append("**") -- Enable searching for files on subdirectories.
-o.smartcase = true -- Use case-sensitive if keyword contains capital letters.
-o.startofline = true -- Move cursor to the first non-blank character.
-o.whichwrap = "b,s,<,>,[,]" -- Move cursor left/right to move to previous/next lines
+o.ignorecase = true -- Ignore case on search patterns. `:h 'ignorecase'`
+o.inccommand = "nosplit" -- Show live preview of substitution. `:h 'inccommand'`
+o.path:append("**") -- Enable searching for files on subdirectories. `:h 'path'`
+o.smartcase = true -- Use case-sensitive if keyword contains capital letters. `:h 'smartcase'`
+o.startofline = true -- Move cursor to the first non-blank character. `:h 'startofline'`
+o.whichwrap = "b,s,<,>,[,]" -- Move cursor left/right to move to previous/next lines `:h 'whichwrap'`
 -- }}}
 
 -- 3 tags {{{
-o.tagstack = true -- Enable tagstack.
+o.tagstack = true -- Enable tagstack. `:h 'tagstack'`
 -- }}}
 
 -- 4 displaying text {{{
-o.cmdheight = 1 -- Show command line on 1 line.
-o.fillchars = {
+o.breakindent = true -- Wrapped lines will continue visually indented `:h 'breakindent'`
+o.cmdheight = 1 -- Show command line on 1 line. `:h 'cmdheight'`
+o.fillchars = { -- `:h 'fillchars'`
 	eob = " ", -- End of buffer marker.
 	diff = vim.fn.is_windows() and "╱" or "⣿", -- Diff deleted lines marker.
 	foldopen = "", -- Beggining of a fold.
@@ -29,51 +30,51 @@ o.fillchars = {
 	fold = " ", -- Filling foldtext.
 	foldsep = " ", -- Open fold middle marker
 }
-o.lazyredraw = true -- Don't redraw when executing macros.
-o.messagesopt = "wait:500,history:1000" -- Option settings for outputting messages.
-o.number = true -- Show line numbers.
-o.numberwidth = 5 -- More space on the gutter column.
-o.relativenumber = true -- Show relative line numbers.
-o.scrolloff = 10 -- Number of lines to keep above/below the cursor.
-o.sidescrolloff = 10 -- Number of columns to keep left/right of the cursor.
-o.wrap = false -- Do not automatically wrap texts.
+o.lazyredraw = true -- Don't redraw when executing macros. `:h 'lazyredraw'`
+o.messagesopt = "wait:500,history:1000" -- Option settings for outputting messages. `:h 'messagesopt'`
+o.number = true -- Show line numbers. `:h 'number'`
+o.numberwidth = 5 -- More space on the gutter column. `:h 'numberwidth'`
+o.relativenumber = true -- Show relative line numbers. `:h 'relativenumber'`
+o.scrolloff = 10 -- Number of lines to keep above/below the cursor. `:h 'scrolloff'`
+o.sidescrolloff = 10 -- Number of columns to keep left/right of the cursor. `:h 'sidescrolloff'`
+o.wrap = false -- Do not automatically wrap texts. `:h 'wrap'`
 -- }}}
 
 -- 5 syntax, highlighting and spelling {{{
-o.cursorline = true -- Highlight current line.
-o.spellfile = vim.fn.stdpath("config") .. "/spell/en.utf-8.add" -- Custom spellfile.
-o.termguicolors = true -- Enable true colours.
+o.cursorline = true -- Highlight current line.`:h 'cursorline'`
+o.spellfile = vim.fn.stdpath("config") .. "/spell/en.utf-8.add" -- Custom spellfile. `:h 'spellfile'`
+o.termguicolors = true -- Enable true colours. `:h 'termguicolors'`
 -- }}}
 
 -- 6 multiple windows {{{
-o.equalalways = false -- Do not resize windows when opening a new one.
-o.laststatus = 3 -- Always show global statusline.
-o.splitbelow = true -- Open a new horizontal split below.
-o.splitright = true -- Open a new vertical split to the right.
-o.statusline = "%!v:lua._G.status_line()" -- statusline format
+o.equalalways = false -- Do not resize windows when opening a new one. `:h 'equalalways'`
+o.laststatus = 3 -- Always show global statusline. `:h 'laststatus'`
+o.splitbelow = true -- Open a new horizontal split below. `:h 'splitbelow'`
+o.splitright = true -- Open a new vertical split to the right. `:h 'splitright'`
+o.statusline = "%!v:lua._G.status_line()" -- statusline format `h 'statusline'`
 -- }}}
 
 -- 7 multiple tab pages {{{
-o.showtabline = 0 -- Never show tabline (tabs are shown on the statusline).
-o.tabclose = "uselast" -- Close tab and go to last used tab.
+o.showtabline = 0 -- Never show tabline (tabs are shown on the statusline). `:h 'showtabline'`
+o.tabclose = "uselast" -- Close tab and go to last used tab. `:h 'tabclose'`
 -- }}}
 
 -- 8 terminal {{{
-o.title = false -- Show info in the window title
-o.titlelen = 1 -- No limit on title length
-o.titlestring = "" -- Show relative path in terminal title
+o.title = false -- Show info in the window title. `:h 'title'`
+o.titlelen = 1 -- No limit on title length. `:h 'titlelen'`
+o.titlestring = "" -- Show relative path in terminal title. `:h 'titlestring'`
 -- }}}
 
 -- 9 using the mouse {{{
-o.mouse = "nic" -- Don't enable mouse on Visual mode.
-o.mousescroll = "ver:1,hor:0" -- Disable horizonal scroll.
+o.mouse = "nic" -- Don't enable mouse on Visual mode. `:h 'mouse'`
+o.mousescroll = "ver:1,hor:0" -- Disable horizonal scroll. `:h 'mousescroll'`
 -- }}}
 
 -- 10 messages and info {{{
-o.belloff = "all" -- Do not ring the bell for any event.
-o.report = 9999 -- Don't report number of changed lines.
-o.ruler = false -- Do not show the line and column number of the cursor position.
-o.shortmess:append({ -- Don't show messages:
+o.belloff = "all" -- Do not ring the bell for any event. `:h 'belloff'`
+o.report = 9999 -- Don't report number of changed lines. `:h 'report'`
+o.ruler = false -- Do not show the line and column number of the cursor position. `:h 'ruler'`
+o.shortmess:append({ -- Don't show messages: `h 'shortmess'`
 	A = true, -- When a swap file is found.
 	C = true, -- When scanning for "ins-completion" items.
 	F = true, -- File info when editing a file.
@@ -84,71 +85,73 @@ o.shortmess:append({ -- Don't show messages:
 	c = true, -- 'ins-completion-menu' messages.
 	s = true, -- Search hit BOTTOM/TOP messages.
 })
-o.showcmd = false -- Do not show command on last line.
-o.showmode = false -- Do not show mode on last line.
+o.showcmd = false -- Do not show command on last line. `:h 'showcmd'`
+o.showmode = false -- Do not show mode on last line. `:h 'showmode'`
 -- }}}
 
 -- 11 selecting text {{{
-o.clipboard = "unnamedplus" -- Use system clipboard.
+vim.schedule(function() -- Schedule the setting after `UiEnter` because it can increase startup-time.
+	o.clipboard = "unnamedplus" -- Use system clipboard. `:h 'clipboard'`
+end)
 -- }}}
 
 -- 12 editing text {{{
-o.complete = ".,]" -- How keyword completion works.
-o.completeopt = "menu,menuone,noinsert,preview" -- Disable native autocompletion (using nvim-cmp).
-o.pumblend = 5 -- Opaque completion menu background.
-o.pumheight = 5 -- Maximum height of pop-up menu.
-o.showmatch = false -- Do not jump to matching brackets.
-o.undofile = true -- Automatically save and restore undo history.
+o.complete = ".,]" -- How keyword completion works. `h 'complete'`
+o.completeopt = "menu,menuone,noinsert,preview" -- Disable native autocompletion (using nvim-cmp). `:h 'completeopt'`
+o.pumblend = 5 -- Opaque completion menu background. `h 'pumblend'`
+o.pumheight = 5 -- Maximum height of pop-up menu. `:h 'pumheight'`
+o.showmatch = false -- Do not jump to matching brackets. `:h 'showmatch'`
+o.undofile = true -- Automatically save and restore undo history. `:h 'undofile'`
 -- }}}
 
 -- 13 tabs and editing {{{
-o.expandtab = true -- In insert mode, use the correct number of spaces to insert a tab.
-o.shiftwidth = 2 -- Number of spaces to use for each step of (auto)indent.
-o.smartindent = true -- Smart indent.
-o.softtabstop = 2 -- Number of spaces that a <Tab> key in the file counts for.
-o.tabstop = 2 -- Number of spaces that a <Tab> in the file counts for.
+o.expandtab = true -- In insert mode, use the correct number of spaces to insert a tab. `:h 'expandtab'`
+o.shiftwidth = 2 -- Number of spaces to use for each step of (auto)indent. `:h 'shiftwidth'`
+o.smartindent = true -- Smart indent. `:h 'smartindent'`
+o.softtabstop = 2 -- Number of spaces that a <Tab> key in the file counts for. `:h 'softtabstop'`
+o.tabstop = 2 -- Number of spaces that a <Tab> in the file counts for. `:h 'tabstop'`
 -- }}}
 
 -- 14 folding {{{
-o.foldcolumn = "1" -- Show folding signs.
-o.foldenable = true -- Enable folding.
-o.foldexpr = "v:lua.vim.treesitter.foldexpr()" -- Use treesitter for folding.
-o.foldlevel = 999 -- Close all folds.
-o.foldlevelstart = 99 -- Start with all folds closed.
-o.foldmethod = "expr" -- Use expr to determine fold level.
-o.foldopen = "insert,mark,search,tag" -- Which commands open folds if the cursor moves into a closed fold.
-o.foldtext = "v:lua.custom_fold_text()" -- What to display on fold
+o.foldcolumn = "1" -- Show folding signs. `:h 'foldcolumn'`
+o.foldenable = true -- Enable folding. `:h 'foldenable'`
+o.foldexpr = "v:lua.vim.treesitter.foldexpr()" -- Use treesitter for folding. `:h 'foldexpr'`
+o.foldlevel = 999 -- Close all folds. `:h 'foldlevel'`
+o.foldlevelstart = 99 -- Start with all folds closed. `:h 'foldlevelstart'`
+o.foldmethod = "expr" -- Use expr to determine fold level. `:h 'foldmethod'`
+o.foldopen = "insert,mark,search,tag" -- Which commands open folds if the cursor moves into a closed fold. `:h 'foldopen'`
+o.foldtext = "v:lua.custom_fold_text()" -- What to display on fold line. `:h 'foldtext'`
 -- }}}
 
 -- 15 diff mode {{{
-o.diffopt = "filler,internal,closeoff,algorithm:histogram,context:5,linematch:60"
+o.diffopt = "filler,internal,closeoff,algorithm:histogram,context:5,linematch:60" -- `:h 'diffopt'`
 -- }}}
 
 -- 16 mapping {{{
-o.timeout = true -- Wait for mapped key sequence to be received `:h 'timeout'`
-o.timeoutlen = 1000 -- Wait 1 second for a possible new key `:h 'timeoutlen'`
+o.timeoutlen = 300 -- Wait time for a possible new key `:h 'timeoutlen'`
+o.ttimeoutlen = 10 -- Wait time for a key code sequence to complete. `:h 'ttimeoutlen'`
 -- }}}
 
 -- 17 reading and writing files {{{
-o.autoread = true -- Automatically reload files changed outside of Vim.
-o.backup = false -- Do not create backup files.
+o.autoread = true -- Automatically reload files changed outside of Vim. `:h 'autoread'`
+o.backup = false -- Do not create backup files. `:h 'backup'`
 -- }}}
 
 -- 18 the swap file {{{
-o.swapfile = false -- Stop creating swp files.
+o.swapfile = false -- Stop creating swp files. `:h 'swapfile'`
 -- }}}
 
 -- 19 command line editing {{{
-o.wildignore:append({ -- Ignore on filename completion.
+o.wildignore:append({ -- Ignore on filename completion. `:h 'wildignore'`
 	".DS_store",
 	"**/node_modules/**",
 })
 -- }}}
 
 -- 24 various {{{
-o.conceallevel = 0 -- Text is shown normally.
-o.gdefault = true -- Use g flag for ":substitute".
-o.shada = {
+o.conceallevel = 0 -- Text is shown normally. `:h 'conceallevel'`
+o.gdefault = true -- Use g flag for ':substitute'. `:h 'gdefault'`
+o.shada = { -- `:h 'shada'`
 	'"50', -- Max number of lines saved for each register.
 	"'50", -- Remember marks for the last 10 edited files.
 	"/50", -- Max number of items in the search pattern.
@@ -159,7 +162,7 @@ o.shada = {
 	"h", -- Disable the effect of hlsearch when loading the shada file.
 }
 -- https://www.reddit.com/r/neovim/comments/1hkpgar/a_per_project_shadafile/
-o.shadafile = (function() -- Per project shadafile
+o.shadafile = (function() -- Per project shadafile `:h 'shadafile'`
 	local data = tostring(vim.fn.stdpath("data"))
 	local git_root = require("snacks.git").get_root()
 	local cwd = git_root or vim.fn.getcwd()
@@ -168,9 +171,9 @@ o.shadafile = (function() -- Per project shadafile
 	vim.fn.mkdir(vim.fs.dirname(file), "p")
 	return file
 end)()
-o.signcolumn = "yes" -- Always showed to prevent the screen from jumping.
-o.viewoptions = "cursor,folds" -- Save cursor position and folds.
-o.winborder = "rounded" -- Use rounded borders for floating windows.
+o.signcolumn = "yes" -- Always showed to prevent the screen from jumping. `:h 'signcolumn'`
+o.viewoptions = "cursor,folds" -- Save cursor position and folds. `:h 'viewoptions'`
+o.winborder = "rounded" -- Use rounded borders for floating windows. `:h 'winborder'`
 -- }}}
 
 -- vim: foldmethod=marker:foldmarker={{{,}}}:foldlevel=0
