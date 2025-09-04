f7fe9ba0cfd0b7a77ed659418203ea3ab6dbf648
Rui Costa <ruicosta042@gmail.com>
2025-09-04 21:05:59 +0100

    fix(fugitive) Jump to first only on BufRead

--- plugin/filetypes/fugitive.lua
+++ plugin/filetypes/fugitive.lua
@@ -1,6 +1,6 @@
 local augroup = vim.api.nvim_create_augroup("ruicsh/filetypes/fugitive", { clear = true })
 
-vim.api.nvim_create_autocmd("BufEnter", {
+vim.api.nvim_create_autocmd("BufRead", {
 	group = augroup,
 	pattern = "fugitive://*",
 	callback = function()
