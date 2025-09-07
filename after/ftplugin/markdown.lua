2d22cc57dd087215c53b89f2b6aaca20925c76fe
Rui Costa <ruicosta042@gmail.com>
2025-09-07 22:36:16 +0100

    fix(markdown) Use treesitter for folding

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,3 +1,4 @@
 vim.opt_local.linebreak = true -- Break lines at word boundaries
 vim.opt_local.textwidth = 0 -- Disable text width
 vim.opt_local.wrap = true -- Enable line wrapping
+vim.opt_local.foldmethod = "expr" -- Enable folding based on expression
