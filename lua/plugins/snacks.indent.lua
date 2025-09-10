715cbad94bb7c3e442ca2f57e5b03329859b24de
Rui Costa <ruicosta042@gmail.com>
2025-09-10 14:26:48 +0100

    fix(indent) Disable for some filetypes

--- lua/plugins/snacks.indent.lua
+++ lua/plugins/snacks.indent.lua
@@ -1,6 +1,16 @@
 -- Indent guides.
 -- https://github.com/folke/snacks.nvim/blob/main/docs/indent.md
 
+local DISABLED_FILETYPES = {
+	"DiffviewFileHistory",
+	"DiffviewFiles",
+	"fugitive",
+	"help",
+	"lazy",
+	"markdown",
+	"terminal",
+}
+
 return {
 	"folke/snacks.nvim",
 	opts = {
@@ -8,6 +18,10 @@ return {
 			animate = {
 				enabled = false,
 			},
+			filter = function(buf)
+				local ft = vim.bo[buf].filetype
+				return vim.tbl_contains(DISABLED_FILETYPES, ft) == false
+			end,
 			scope = {
 				only_current = true,
 			},
