b8f72581d56fa3e0c79ed275bf4be1a8e5246292
Rui Costa <ruicosta042@gmail.com>
2025-09-15 09:59:31 +0100

    feat(shell) Add support for Windows Command Prompt

--- plugin/shell.lua
+++ plugin/shell.lua
@@ -1,6 +1,10 @@
-if vim.fn.env_get("SHELL") == "powershell" then
+local env_shell = vim.fn.env_get("SHELL")
+
+local options = {}
+
+if env_shell == "powershell" then -- Windows PowerShell
 	-- `:h shell-powershell`
-	local powershell_options = {
+	options = {
 		shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
 		shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
 		shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
@@ -8,8 +12,17 @@ if vim.fn.env_get("SHELL") == "powershell" then
 		shellquote = "",
 		shellxquote = "",
 	}
+elseif env_shell == "cmd" then -- Windows Command Prompt
+	options = {
+		shell = "C:\\Windows\\System32\\cmd.exe",
+		shellcmdflag = "/c",
+		shellredir = ">%s 2>&1",
+		shellpipe = "2>&1 | tee %s",
+		shellquote = '"',
+		shellxquote = "",
+	}
+end
 
-	for option, value in pairs(powershell_options) do
-		vim.opt[option] = value
-	end
+for option, value in pairs(options) do
+	vim.opt[option] = value
 end
