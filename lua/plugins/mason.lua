3d60f3617ff6641c94471307cd7aaf4b4552e8b5
Rui Costa <ruicosta042@gmail.com>
2025-09-17 13:24:58 +0100

    fix(mason) Don't change PATH, it's breaking terminal on windows

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -110,8 +110,6 @@ end
 return {
 	"williamboman/mason.nvim",
 	init = function()
-		-- Make mason packages available before loading it; allows to lazy-load mason.
-		vim.env.PATH = vim.fn.stdpath("data") .. "/mason/bin:" .. vim.env.PATH
 		-- Do not crowd home directory with NPM cache folder
 		vim.env.npm_config_cache = vim.env.HOME .. "/.cache/npm"
 	end,
