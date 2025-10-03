e1c0ef8c273f6b43d1fca522cdcd2086ca60a5ee
Rui Costa <ruicosta042@gmail.com>
2025-10-04 00:55:52 +0100

    fix(qf) Use default keymaps

--- after/ftplugin/qf.lua
+++ after/ftplugin/qf.lua
@@ -1,14 +1,3 @@
 vim.opt_local.relativenumber = false
 vim.opt_local.statusline = "%!v:lua._G.status_line_qf()"
 vim.opt_local.cursorline = true
-
-vim.keymap.set("n", "<c-j>", "j", { desc = "Next quickfix item", buffer = true })
-vim.keymap.set("n", "<c-k>", "k", { desc = "Previous quickfix item", buffer = true })
-
-vim.keymap.set("n", "<cr>", function()
-	local item = vim.fn.getqflist()[vim.fn.line(".")]
-	if item and item.valid == 1 then
-		vim.cmd("cc " .. vim.fn.line("."))
-		vim.cmd("cclose")
-	end
-end, { buffer = true, desc = "Open item and close quickfix" })
