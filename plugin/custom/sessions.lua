6593ebd5e3f5e0336c742bafe64e0f3ced6a1cca
Rui Costa <ruicosta042@gmail.com>
2025-10-24 11:22:23 +0100

    fix(sessions) Close diffview files before saving

--- plugin/custom/sessions.lua
+++ plugin/custom/sessions.lua
@@ -73,7 +73,7 @@ local function close_special_buffers()
 	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
 		if vim.api.nvim_buf_is_loaded(buf) then
 			local name = vim.api.nvim_buf_get_name(buf)
-			if name:match("^fugitive://") then
+			if name:match("^fugitive://") or name:match("^diffview://") then
 				vim.api.nvim_buf_delete(buf, { force = true })
 			end
 		end
