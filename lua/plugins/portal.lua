a78bf60e01d3a415759421bfe24e26938a7c4097
Rui Costa <ruicosta042@gmail.com>
2025-10-26 01:25:45 +0000

    fix(portal) use [[]] to navigate jumplist

--- lua/plugins/portal.lua
+++ lua/plugins/portal.lua
@@ -5,19 +5,13 @@ return {
 	"cbochs/portal.nvim",
 	keys = function()
 		local mappings = {
-			{
-				"<leader>[",
-				":Portal jumplist backward<cr>",
-				"Backward",
-			},
-			{
-				"<leader>]",
-				":Portal jumplist forward<cr>",
-				"Forward",
-			},
+			{ "[[", ":Portal jumplist backward<cr>", "Backward" },
+			{ "]]", ":Portal jumplist forward<cr>", "Forward" },
 		}
 
 		return vim.fn.get_lazy_keys_conf(mappings, "Portal")
 	end,
-	opts = {},
+	opts = {
+		labels = { "a", "s", "d", "f" },
+	},
 }
