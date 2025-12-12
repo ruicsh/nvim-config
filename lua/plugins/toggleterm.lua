62153c9d1f206d305ef7158337e09ad0a64e300f
Rui Costa <ruicosta042@gmail.com>
2025-12-12 10:53:10 +0000

    fix(terminal) Use toggleterm.on_open to show left padding

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -6,15 +6,21 @@ return {
 	opts = {
 		direction = "float",
 		float_opts = {
+			anchor = "NE",
 			border = { "", "", "", "", "", "", "", "│" },
-			width = math.floor(vim.o.columns * 0.5),
+			col = vim.o.columns,
 			height = vim.o.lines - vim.o.cmdheight - 1,
+			relative = "editor",
 			row = 0,
-			col = math.floor(vim.o.columns * 0.5) + 1,
+			style = "minimal",
+			width = math.floor(vim.o.columns * 0.5),
 		},
 		highlights = {
 			FloatBorder = { link = "FloatBorder" },
 		},
+		on_open = function()
+			vim.opt_local.signcolumn = "yes" -- Show left padding on the window
+		end,
 		open_mapping = "<c-t>",
 	},
 }
