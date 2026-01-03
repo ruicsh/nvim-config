1d220d443a38dbef19bc2e4c5e5310fc33def897
Rui Costa <ruicosta042@gmail.com>
2026-01-03 17:38:21 +0000

    fix(yanky) Trim some useless keymaps

--- lua/plugins/yanky.lua
+++ lua/plugins/yanky.lua
@@ -4,17 +4,17 @@
 return {
 	"gbprod/yanky.nvim",
 	keys = {
-		{ "<leader>p", "<cmd>lua Snacks.picker.yanky()<cr>", desc = "Clipboard: History", mode = { "n", "x" } },
+		{ "<leader>p", "<cmd>lua Snacks.picker.yanky()<cr>", desc = "Clipboard: History" },
 		{ "y", "<Plug>(YankyYank)", desc = "Clipboard: Yank", mode = { "n", "x" } },
 		{ "p", "<Plug>(YankyPutAfter)", desc = "Clipboard: Put after", mode = { "n", "x" } },
-		{ "<c-v>", "<Plug>(YankyPutAfter)", desc = "Clipboard: Put after", mode = { "n", "x" } },
 		{ "P", "<Plug>(YankyPutBefore)", desc = "Clipboard: Put before", mode = { "n", "x" } },
+		{ "gp", "<Plug>(YankyGPutAfter)", desc = "Clipboard: Put after (cursor)", mode = { "n", "x" } },
+		{ "gP", "<Plug>(YankyGPutBefore)", desc = "Clipboard: Put before (cursor)", mode = { "n", "x" } },
+		{ "<c-v>", "<Plug>(YankyPutAfter)", desc = "Clipboard: Put after", mode = { "n", "x" } },
 		{ "<c-p>", "<Plug>(YankyPreviousEntry)", desc = "Clipboard: Select previous" },
 		{ "<c-n>", "<Plug>(YankyNextEntry)", desc = "Clipboard: Select next" },
 		{ "]p", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Clipboard: Put after (linewise)" },
 		{ "[p", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Clipboard: Put before (linewise)" },
-		{ "]P", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Clipboard: Put after (linewise)" },
-		{ "[P", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Clipboard: Put before (linewise)" },
 	},
 	opts = {
 		highlight = {
