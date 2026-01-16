83473b65b24fb990a85bd6c2a273a540c4ed1de1
ruicsh <ruicosta042@gmail.com>
2026-01-16 11:35:21 +0000

    feat(oil) Show git-ignored as hidden files

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,6 +1,35 @@
 -- Directory editor
 -- https://github.com/stevearc/oil.nvim
 
+-- Helper function to parse output
+local function parse_output(proc)
+	local result = proc:wait()
+	local ret = {}
+	if result.code == 0 then
+		for line in vim.gsplit(result.stdout, "\n", { plain = true, trimempty = true }) do
+			-- Remove trailing slash
+			line = line:gsub("/$", "")
+			ret[line] = true
+		end
+	end
+	return ret
+end
+
+-- Build git status cache
+local function new_git_status_ignored()
+	return setmetatable({}, {
+		__index = function(self, key)
+			local ignore_proc = vim.system(
+				{ "git", "ls-files", "--ignored", "--exclude-standard", "--others", "--directory" },
+				{ cwd = key, text = true }
+			)
+			local ret = parse_output(ignore_proc)
+			rawset(self, key, ret)
+			return ret
+		end,
+	})
+end
+
 return {
 	"stevearc/oil.nvim",
 	keys = function()
@@ -50,6 +79,28 @@ return {
 			signcolumn = "yes:2",
 		},
 	},
+	config = function(_, opts)
+		local oil = require("oil")
+		local actions = require("oil.actions")
+
+		local git_status_ignored = new_git_status_ignored()
+
+		-- Clear git status cache on refresh
+		local refresh = actions.refresh
+		local orig_refresh = refresh.callback
+		refresh.callback = function(...)
+			git_status_ignored = new_git_status_ignored()
+			orig_refresh(...)
+		end
+
+		-- Show git-ignored files as hidden
+		opts.view_options.is_hidden_file = function(name, bufnr)
+			local dir = oil.get_current_dir(bufnr)
+			return git_status_ignored[dir][name]
+		end
+
+		oil.setup(opts)
+	end,
 
 	dependencies = {
 		{ "nvim-tree/nvim-web-devicons" },
