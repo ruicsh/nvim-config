86d088b8ebd655a93dc6320299be60ad30ac2066
ruicsh <ruicosta042@gmail.com>
2026-02-06 20:00:20 +0000

    fix(tailwind) add keymap to toggle conceal classNames

--- after/ftplugin/typescriptreact.lua
+++ after/ftplugin/typescriptreact.lua
@@ -1,1 +1,7 @@
 vim.opt_local.conceallevel = 1 -- Conceal Tailwind CSS classes
+
+-- Toggle Tailwind class conceal
+vim.keymap.set("n", "<leader>tt", function()
+	local cl = vim.api.nvim_get_option_value("conceallevel", { scope = "local" })
+	vim.opt_local.conceallevel = cl == 1 and 0 or 1
+end, { buffer = true, desc = "Toggle Tailwind class conceal" })
