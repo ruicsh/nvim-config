53a16a491aed0e865c568d53278f833ca00aaebc
ruicsh <ruicosta042@gmail.com>
2026-02-15 18:57:02 +0000

    fix(git) use fugitive for selection range history

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,6 +1,8 @@
 -- Git client
 -- https://github.com/tpope/vim-fugitive
 
+local T = require("lib")
+
 return {
 	"tpope/vim-fugitive",
 	keys = function()
@@ -19,9 +21,20 @@ return {
 			vim.fn.search(pattern, "w")
 		end
 
+		local function show_range_history()
+			local start_line = vim.fn.line("v")
+			local end_line = vim.fn.line(".")
+			local file_path = vim.fn.expand("%")
+			local range = string.format("%s,%s", start_line, end_line)
+			local cmd = string.format("Git log -L %s:%s", range, file_path)
+
+			T.ui.open_side_panel({ cmd = cmd, mode = "replace" })
+		end
+
 		return {
-			{ "<leader>hH", git_status, desc = "Git: Status" },
 			{ "<leader>hd", git_diff_file, desc = "Git: Diff file" },
+			{ "<leader>hH", git_status, desc = "Git: Status" },
+			{ "<leader>hl", show_range_history, mode = "v", desc = "Git: Show range history" },
 		}
 	end,
 
