ec2681283add937986e84195b9246ecf92746e89
ruicsh <ruicosta042@gmail.com>
2026-02-24 18:34:38 +0000

    fix(terminal) show padding on the left of window

---
+++ plugin/filetypes/terminal.lua
@@ -0,0 +1,10 @@
+-- Terminal buffers
+
+local augroup = vim.api.nvim_create_augroup("ruicsh/autocmds/terminal", { clear = true })
+
+vim.api.nvim_create_autocmd("TermOpen", {
+	group = augroup,
+	callback = function()
+		vim.opt_local.signcolumn = "yes" -- Shows left padding on the window
+	end,
+})
