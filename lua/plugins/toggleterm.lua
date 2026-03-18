eb6c7ced5cff2f6b8c368df93c007a568506d66f
ruicsh <ruicosta042@gmail.com>
2026-03-18 22:24:16 +0000

    fix(terminal) map <c-[> to exit terminal mode

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -11,16 +11,6 @@ return {
 		highlights = {
 			FloatBorder = { link = "FloatBorder" },
 		},
-		on_open = function(term)
-			vim.opt_local.signcolumn = "yes" -- Show left padding on the window
-
-			local k = function(lhs, rhs, options)
-				local opts = vim.tbl_extend("force", { buffer = term.bufnr }, options or {})
-				vim.keymap.set("t", lhs, rhs, opts)
-			end
-
-			k("<c-[>", [[<c-\><c-n>]], { desc = "Exit terminal mode" })
-		end,
 		open_mapping = "<c-bslash>",
 		persist_mode = false,
 	},
--- plugin/filetypes/terminal.lua
+++ plugin/filetypes/terminal.lua
@@ -6,5 +6,10 @@ vim.api.nvim_create_autocmd("TermOpen", {
 	group = augroup,
 	callback = function()
 		vim.opt_local.signcolumn = "yes" -- Shows left padding on the window
+
+		-- opencode uses <esc> to cancel actions, map it when not in a sidekick terminal
+		if vim.bo.filetype ~= "sidekick_terminal" then
+			vim.keymap.set("t", "<c-[>", [[<c-\><c-n>]], { desc = "Exit terminal mode", buffer = true })
+		end
 	end,
 })
