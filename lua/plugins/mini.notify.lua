fb908788616d26755efaab26966a335d9f0e74ef
ruicsh <ruicosta042@gmail.com>
2026-03-30 12:46:58 +0100

    fix(mini.notify) filter out pyright lsp progress messages

--- lua/plugins/mini.notify.lua
+++ lua/plugins/mini.notify.lua
@@ -3,23 +3,37 @@
 
 return {
 	"nvim-mini/mini.notify",
-	opts = {
-		content = {
-			format = nil,
-			sort = nil,
-		},
-		lsp_progress = {
-			enable = true,
-			duration_last = 3000,
-		},
-		window = {
-			config = {
-				border = "rounded",
+	opts = function()
+		local MiniNotify = require("mini.notify")
+
+		return {
+			content = {
+				format = nil,
+				sort = function(notif_arr)
+					-- Filter out Pyright LSP progress notifications
+					notif_arr = vim.tbl_filter(function(notif)
+						return not (
+							notif.data
+							and notif.data.source == "lsp_progress"
+							and notif.data.client_name == "pyright"
+						)
+					end, notif_arr)
+					return MiniNotify.default_sort(notif_arr)
+				end,
 			},
-			max_width_share = 0.382,
-			winblend = 0,
-		},
-	},
+			lsp_progress = {
+				enable = true,
+				duration_last = 3000,
+			},
+			window = {
+				config = {
+					border = "rounded",
+				},
+				max_width_share = 0.382,
+				winblend = 0,
+			},
+		}
+	end,
 
 	event = { "VeryLazy" },
 }
